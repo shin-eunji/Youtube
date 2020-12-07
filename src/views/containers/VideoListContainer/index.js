24873d4d6b5c0dd27fde0f63dab381edb200c689
shin-eunji <dmswlckato@gmail.com>
2020-12-07 22:56:24 +0900

    container

---
+++ src/views/containers/VideoListContainer/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function VideoListContainer (props) {
+
+    const {} = props;
+
+    return (
+        <Container>
+            VideoListContainer
+        </Container>
+    )
+}
+
+const Container = styled.div`
+`
+export default VideoListContainer;
